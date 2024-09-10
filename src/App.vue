7abb0f06d381ec1735a10cbaa2e9a90730092c5a
Rizky Zanuar Maulana Haj <rmaul632@gmail.com>
2024-09-10 11:52:39 +0700

    complete

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <!-- Button Counter -->
+    <button @click="incrementCounter">Klik Saya!</button>
+    <p>Counter: {{ counter }}</p>
+
+    <!-- Box dengan warna -->
+    <div :style="{ backgroundColor: boxColor, width: '100px', height: '100px', marginTop: '20px' }"></div>
+    <button @click="changeBoxColor">Ubah Warna Box</button>
+
+    <!-- Image Carousel -->
+    <div class="carousel">
+      <button @click="prevImage">back</button>
+      <img :src="images[currentImage]" alt="Image Carousel" class="carousel-image">
+      <button @click="nextImage">lanjut</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+      boxColor: 'lightblue',
+      images: [
+        'https://via.placeholder.com/300?text=Image+1',
+        'https://via.placeholder.com/300?text=Image+2',
+        'https://via.placeholder.com/300?text=Image+3'
+      ],
+      currentImage: 0
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter += 1;
+    },
+    changeBoxColor() {
+      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      this.boxColor = randomColor;
+    },
+    prevImage() {
+      this.currentImage = (this.currentImage - 1 + this.images.length) % this.images.length;
+    },
+    nextImage() {
+      this.currentImage = (this.currentImage + 1) % this.images.length;
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  font-size: 16px;
+  padding: 10px;
+  margin: 10px;
+}
+p {
+  font-size: 20px;
+}
+.carousel {
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+}
+.carousel-image {
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+  margin: 0 10px;
+}
+</style>
